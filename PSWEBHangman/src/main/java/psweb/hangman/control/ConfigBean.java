accc50e342be7e409abc36c8944fa8092acbb15e
padu <padu77@gmail.com>
2018-05-15 01:16:44 -0300

    Aplicação dos ajustes feitos durante a aula

--- PSWEBHangman/src/main/java/psweb/hangman/control/ConfigBean.java
+++ PSWEBHangman/src/main/java/psweb/hangman/control/ConfigBean.java
@@ -12,43 +12,50 @@ import psweb.hangman.model.Tipo;
 
 /**
  * Classe bean para a configuração do jogo
- * @author Paulo Cantuária e Augusto
+ * 
+ * @author Paulo Cantuária e Augusto 
  * @since 1.0
  */
 @Component
 @ManagedBean
 @SessionScoped
-public class ConfigBean {
-	
-	private Dificuldade dificuldade;	
-	private List<Player> players;	
+public class ConfigBean extends _Bean {
+
+	private Dificuldade dificuldade;
+	private List<Player> players;
 	private Tipo tipo; // Um jogador / dois jogadores
 	private int qtdPArtidas;
+
 	public Dificuldade getDificuldade() {
 		return dificuldade;
 	}
+
 	public void setDificuldade(Dificuldade dificuldade) {
 		this.dificuldade = dificuldade;
 	}
+
 	public List<Player> getPlayers() {
 		return players;
 	}
+
 	public void setPlayers(List<Player> players) {
 		this.players = players;
 	}
+
 	public Tipo getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(Tipo tipo) {
 		this.tipo = tipo;
 	}
+
 	public int getQtdPArtidas() {
 		return qtdPArtidas;
 	}
+
 	public void setQtdPArtidas(int qtdPArtidas) {
 		this.qtdPArtidas = qtdPArtidas;
 	}
-	
-	
 
 }
--- PSWEBHangman/src/main/java/psweb/hangman/control/Dificuldade.java
+++ PSWEBHangman/src/main/java/psweb/hangman/control/Dificuldade.java
@@ -1,5 +1,20 @@
 package psweb.hangman.control;
 
 public enum Dificuldade {
+	FACIL("Fácil"), MEDIO("Médio"), DIFICIL("Difícil");
 
+	private final String dificuldade;
+
+	Dificuldade(String dificuldade) {
+		this.dificuldade = dificuldade;
+	}
+
+	public static Dificuldade[] asList() {
+		return Dificuldade.values();
+	}
+
+	public String getDificuldade() {
+		return dificuldade;
+	}
 }
+
