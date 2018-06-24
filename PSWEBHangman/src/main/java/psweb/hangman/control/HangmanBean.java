8789a93c555cb0e1a3d3eba6321b5b1026a00072
Paulo Eduardo Cantuária <padu77@gmail.com>
2018-06-24 17:24:18 -0300

    Update HangmanBean.java

--- PSWEBHangman/src/main/java/psweb/hangman/control/HangmanBean.java
+++ PSWEBHangman/src/main/java/psweb/hangman/control/HangmanBean.java
@@ -13,7 +13,8 @@ import org.springframework.stereotype.Component;
 
 import psweb.hangman.model.Hangman;
 import psweb.hangman.model.Player;
-import psweb.hangman.services.PlayerServices;
+import psweb.hangman.services.PlayerServicesCasta;
+import psweb.hangman.services.WordServices;
 import psweb.hangman.utils.enums.Dificuldade;
 
 /**
@@ -40,6 +41,10 @@ public class HangmanBean extends _Bean {
 
 	@Autowired
 	public ConfigBean config;
+	@Autowired
+	private PlayerServicesCasta playerServices;
+	@Autowired
+	private WordServices wordServices;
 
 	//
 	// Campos do Formulário
@@ -57,24 +62,36 @@ public class HangmanBean extends _Bean {
 	public ConfigBean getConfigBean() {
 		return config;
 	}
-	
+
 	public void configPlayers() {
 		// seta o player1
 		currentPlayer = config.getPlayer1();
+		Player p1PreExist = null;
+
+		if (currentPlayer.getName().trim().isEmpty() || currentPlayer.getName() == null) {
+			currentPlayer.setName("Player 1");
+		}
+		p1PreExist = playerServices.retrieveByNome(currentPlayer.getName());
+		if (p1PreExist != null) {
+			currentPlayer.setId(p1PreExist.getId());
+		}
 		currentPlayer.setCurrentScore(0);
-		
+
 		// seta o player2 se for o caso e zera o scores
-		if(config.isTwoPlaying()) {
-			currentEnemy =  config.getPlayer2();
+		if (config.isTwoPlaying()) {
+			currentEnemy = config.getPlayer2();
 			currentEnemy.setCurrentScore(0);
+			if(config.getPlayer2().getName().trim().isEmpty() || config.getPlayer2().getName() == null) {
+				config.getPlayer2().setName("Player 2");
+			}
 		}
 	}
-	
+
 	public void setDifficulty(ActionEvent event) {
 		config.setDifficulty(event);
 		firstReset();
 	}
-	
+
 	/**
 	 * Método para configuração do jogo na primeira inicialização
 	 * 
@@ -82,51 +99,58 @@ public class HangmanBean extends _Bean {
 	public void firstReset() {
 		this.setSoundPlaying(true);
 		this.setInGame(true);
-		
+
 		// prepara o jogo para iniciar
 		reset();
 	}
-	
+
 	public void reset() {
 		letter = "";
 		hint = "";
 		currentHint = "";
-		
-		if(!config.isTwoPlaying())
+
+		if (!config.isTwoPlaying())
 			hangman.reset(config.getDificuldade());
 		else
 			hangman.reset(wordFromPlayer);
 	}
-	
+
+	private void resetTwoPlayers() {
+		// abre modal para currentEnemy digitar palavra
+		String wordFromPlayer = "TESTANDO"; // TODO implementar modal para player digitar a palavra
+
+		// seta palavra digitada e inicia o jogo
+		hangman.reset(wordFromPlayer);
+	}
+
 	private void finishGame() {
 		// calcula o score obtido pelo jogador
 		int score = computeScore();
-		
+
 		// atualiza o score do current player
-		if(score != 0) {
+		if (score != 0) {
 			saveScoreToCurrentPlayer(score);
 		}
-		
+
 		// se dois jogadores, alterna current player e enemy
-		if(config.isTwoPlaying())
+		if (config.isTwoPlaying())
 			togglePlayers();
-		
+
 		// redireciona para página de fim de jogo
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("endgame.xhtml");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// abre modal de fim de jogo
-		//Map<String,Object> options = new HashMap<String, Object>();
-        //options.put("modal", true);
-        //PrimeFaces.current().dialog.openDynamic("dlgEndGame", options, null);
-        //RequestContext.getCurrentInstance().openDialog("dlgEndGame", options, null);
-		
+		// Map<String,Object> options = new HashMap<String, Object>();
+		// options.put("modal", true);
+		// PrimeFaces.current().dialog.openDynamic("dlgEndGame", options, null);
+		// RequestContext.getCurrentInstance().openDialog("dlgEndGame", options, null);
+
 	}
-	
+
 	/**
 	 * Computa o score obtido na rodada
 	 * 
@@ -134,56 +158,57 @@ public class HangmanBean extends _Bean {
 	 */
 	private int computeScore() {
 		// se perdeu, retorna 0;
-		if(isGameLose())
+		if (isGameLose())
 			return 0;
-		
+
 		// se não perdeu e jogo está no modo TWOPLAYERS
-		if(config.isTwoPlaying())
+		if (config.isTwoPlaying())
 			return isGameWin() ? 1 : 0;
-		
+
 		// se não perdeu e jogo está no modo ONEPLAYER
 		return computeScoreFromDificulty();
 	}
 
 	private int computeScoreFromDificulty() {
 		int score = 0;
-		
+
 		Dificuldade dificulty = config.getDificuldade();
 		switch (dificulty) {
-			case EASY:
-				score = 2;
-				break;
-			case NORMAL:
-				score = 3;
-				break;
-			case HARD:
-				score = 4;
-				break;
+		case EASY:
+			score = 2;
+			break;
+		case NORMAL:
+			score = 3;
+			break;
+		case HARD:
+			score = 4;
+			break;
 		}
 
 		if (isHintDisplayed()) {
 			score--;
 		}
-		
+
 		return score;
 	}
-	
+
 	/**
 	 * Atualiza o score do currentPlayer e persiste no banco se for modo ONEPLAYER
 	 * 
-	 * @param score (int) score obtido na rodada.
+	 * @param score
+	 *            (int) score obtido na rodada.
 	 */
 	private void saveScoreToCurrentPlayer(int score) {
 		// atualiza o score atual
 		currentPlayer.setCurrentScore(currentPlayer.getCurrentScore() + score);
-		
+
 		// se for modo ONEPLAYER, atualiza o score acumulado no banco
-		if(!config.isTwoPlaying()) {
+		if (!config.isTwoPlaying()) {
 			currentPlayer.setScore(currentPlayer.getScore() + score);
-			PlayerServices.insert(currentPlayer);
+			playerServices.saveUpdate(currentPlayer);
 		}
 	}
-	
+
 	/**
 	 * Alterna os players no modo TWOPLAYERS
 	 * 
@@ -196,20 +221,17 @@ public class HangmanBean extends _Bean {
 		System.out.println(config.getPlayer2().getName());
 		System.out.print("Jogador corrente: ");
 		System.out.println(currentPlayer.getName());
-		
-		if(currentPlayer.equals(config.getPlayer1())) {
+
+		if (currentPlayer.equals(config.getPlayer1())) {
 			System.out.println("Jogador corrente == Player1");
 			currentPlayer = config.getPlayer2();
 			currentEnemy = config.getPlayer1();
-		}
-		else {
+		} else {
 			System.out.println("Jogador corrente != Player1");
 			currentPlayer = config.getPlayer1();
 			currentEnemy = config.getPlayer2();
 		}
 	}
-	
-	
 
 	//
 	// Operações
@@ -218,17 +240,13 @@ public class HangmanBean extends _Bean {
 		char chr = letter.toCharArray()[0];
 		hangman.input(chr);
 		letter = "";
-		if(!config.isTwoPlaying()) {
-			if (currentHint != hangman.getTrueHint()) {
-				hint = "";
-			}
+		if (currentHint != hangman.getTrueHint()) {
+			hint = "";
 		}
-		if(isGameOver())
+		if (isGameOver())
 			finishGame();
 	}
-	
 
-	
 	public void showHint() {
 		currentHint = hangman.getHint();
 		hint = currentHint == hangman.getTrueHint() ? "Hint: " + currentHint : "No Hint: " + currentHint;
@@ -250,7 +268,7 @@ public class HangmanBean extends _Bean {
 	public String getWord() {
 		return hangman.getWordAsString();
 	}
-	
+
 	public String getAnswer() {
 		return hangman.getAnswerAsString();
 	}
@@ -300,7 +318,7 @@ public class HangmanBean extends _Bean {
 	public void setCurrentPlayer(Player currentPlayer) {
 		this.currentPlayer = currentPlayer;
 	}
-	
+
 	public Player getCurrentEnemy() {
 		return currentEnemy;
 	}
@@ -324,7 +342,7 @@ public class HangmanBean extends _Bean {
 	public boolean isSoundPlaying() {
 		return isSoundPlaying;
 	}
-	
+
 	public void setSoundPlaying(boolean soundPlaying) {
 		this.isSoundPlaying = soundPlaying;
 	}
@@ -344,5 +362,5 @@ public class HangmanBean extends _Bean {
 	public void setWordFromPlayer(String wordFromPlayer) {
 		this.wordFromPlayer = wordFromPlayer;
 	}
-	
+
 }
